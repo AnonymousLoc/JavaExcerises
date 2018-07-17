ef3fa1fd52a2511f0545f215a815d8ad869442b5
Arjun <arjun.gandhi@academytrainee.com>
2018-07-17 15:53:57 +0100

    ex updated commit

--- example1/src/example1/ex1_parameters.java
+++ example1/src/example1/ex1_parameters.java
@@ -1,15 +1,40 @@
 package example1;
 
+import java.util.ArrayList;
+
 public class ex1_parameters {
 	
 	public static void main(String [] args) {
-		Person s1=new Person("JOhn ",24, "Engineer");	
-System.out.println(s1);
+		
+//System.out.println(s1);
 	
+	search();
+	
+	
+
+		
 	}
 	
-	
-}	
+	static void search() {
+		Person p1=new Person("John",24, "Engineer");	
+		Person p2=new Person("Adam",22, "Trainee");
+		Person p3=new Person("Luke",27, "Nothing");
+		Person p4=new Person("Mark",29, "Loser");
+
+		ArrayList <Person> people = new ArrayList<Person>();
+		people.add(p1);
+		people.add(p2);
+		people.add(p3);
+		people.add(p4);
+		
+	for (Person i :people) {
+		if (i.name=="Mark") {
+			System.out.println(i);	
+		}
+
+	}	
+	}	
+}
 	
 	
 	
